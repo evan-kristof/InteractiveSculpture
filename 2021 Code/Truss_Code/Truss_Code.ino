25f045dedd504675ec8e7a9329a4e10cb770e65c
Omar Waked <waked@icloud.com>
2021-05-20 12:52:31 -0700

    Updated code to account for multiple load cells, easier to read serial monitor output

--- 2021 Code/Truss_Code/Truss_Code.ino
+++ 2021 Code/Truss_Code/Truss_Code.ino
@@ -5,7 +5,7 @@
  * Truss_Code.ino
  * 
  * DATE CREAED: 1/29/21 BY OMAR WAKED
- * DATE MODIFIED: 1/29/21 BY OMAR WAKED
+ * DATE MODIFIED: 5/18/21 BY Evan Snow
  * IMPORTANT: UPDATE THE MODIFIED DATE AND NAME ABOVE 
  * LINK TO CODE ON GITHUB: https://github.com/OmarWaked/InteractiveSculpture.git
 */
@@ -13,33 +13,26 @@
 //IMPORT FILES REQUIRED FOR CODE IN THIS FILE TO WORK
 #include "HX711.h"
 #include "config.h"
+#include <LiquidCrystal.h>
 
 //DEFINE VARIABLES
 //load cells
+
 HX711 load_cell1 (LOADCELL_DT_PIN1, LOADCELL_SCK_PIN1);
 HX711 load_cell2 (LOADCELL_DT_PIN2, LOADCELL_SCK_PIN2);
 HX711 load_cell3 (LOADCELL_DT_PIN3, LOADCELL_SCK_PIN3);
-<<<<<<< Updated upstream
+
 //other variables
+float SCALE = 19270.0; // for the larger S-type load cell SCALE should be about 19470, for smaller round load cell SCALE should be about 27470
 unsigned long lastUpdate = 0; //passive delay (keeps track of elapsed time)
 int buttonState = 0;   // variable for reading the pushbutton status (0 = button not pressed, 1 = button pressed)
 float massReading = 0;
 //float SCALE = 20000.0;
-float MIN_LB = -5.00;
-float MAX_LB = 5.00;
-float MAP_LOW = 0;
-=======
-
-//HX711 LOAD CELL CALIBRATION FACTOR 
-float SCALE = 19270.0; // for the larger S-type load cell SCALE should be about 19470, for smaller round load cell SCALE should be about 27470
 
 //LCD pins
 const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-//Timer
-unsigned long lastUpdate = 0; //passive delay (keeps track of elapsed time)
->>>>>>> Stashed changes
 
 //THIS FUNCTION WILL RUN ONCE WHEN THE ARDUINO IS POWERED UP
 void setup() {
@@ -48,44 +41,34 @@ void setup() {
     pinMode(GREEN_LED, OUTPUT);                      //Output for the LED lights
     pinMode(BLUE_LED, OUTPUT);                       //Output for the LED lights
     delay(1000);                                     //Allow Arduino to properly setup
+    
     setupCell(load_cell1, "CELL1");
     load_cell1.tare();                               // reset the scale to 0 for scale 1
-<<<<<<< Updated upstream
-=======
     setupCell(load_cell2, "CELL2");
     load_cell2.tare();                               // reset the scale to 0 for scale 1
-    setupCell(load_cell3, "CELL3");
-    load_cell3.tare();                               // reset the scale to 0 for scale 1
+//    setupCell(load_cell3, "CELL3");
+//    load_cell3.tare();                               // reset the scale to 0 for scale 1
     
     setupLCD();
->>>>>>> Stashed changes
 }
 
 //THIS FUNCTION WILL RUN FOREVER UNLESS INSTRUCTED TO STOP
-void loop() {
-<<<<<<< Updated upstream
-    //set_calibration(load_cell1);
-    read_send(load_cell1);
-}
+// the loop function first checks if the calibration button is being pushed. if yes, it runs the set_calibration function which sets a new scale factor.
+// if the button is not being pushed it moves on to the read_send function which is the primary part of the loop function.
 
-//intialize load cells 
-void setupCell(HX711 load_cellX){
-    //load_cellX.set_scale(SCALE);
-    Serial.println("test after calibration:");
-    Serial.print("read: \t\t");
-=======
-    read_send(load_cell1, "CELL1");
-    read_send(load_cell2, "CELL2");
-    read_send(load_cell3, "CELL3");
-//    if (digitalRead(buttonPin) == HIGH){
-//      set_calibration(load_cell1);
-//      set_calibration(load_cell2);
-//      set_calibration(load_cell3);
-//    }
+void loop() {
+  read_send(load_cell1, "CELL1");
+  read_send(load_cell2, "CELL2");
+//  read_send(load_cell3, "CELL3");
+    
+  if (digitalRead(buttonPin) == HIGH){
+   set_calibration(load_cell1);
+   set_calibration(load_cell2);
+//   set_calibration(load_cell3);
+  }
 }
 
 // Functions below here are subsidiary to the above setup() and loop() functions
-
 
 //intialize load cells. this function is not required for operation of the load cell, it just prints out some basic info to make sure the arduino is communicating with the load cell 
 //the numbers printed out are raw data from the load cell, not converted to lbs
@@ -95,21 +78,12 @@ void setupCell(HX711 load_cellX, String cellID){
     Serial.println();
     load_cellX.set_scale(SCALE);
     Serial.print("read: \t\t\t");
->>>>>>> Stashed changes
     Serial.println(load_cellX.read());                // print a raw reading from the ADC for scale 1
     Serial.print("read average: \t\t");
     Serial.println(load_cellX.read_average(20));      // print the average of 20 readings from the ADC for scale 1
     Serial.print("get value: \t\t");
     Serial.println(load_cellX.get_value(5));          // print the average of 5 readings from the ADC minus the tare weight, set with tare() for scale 1
     Serial.print("get units: \t\t");
-<<<<<<< Updated upstream
-    Serial.println(load_cellX.get_units(5), 1);       // print the average of 5 readings from the ADC minus tare weight, divided by scale parameter wit set_scale
-}
-
-//read inputs from load cell and control LEDs accordinly 
-void read_send(HX711 load_cellX){
-    float SCALE = 27470.0;
-=======
     Serial.println(load_cellX.get_units(5));       // print the average of 5 readings from the ADC minus tare weight, divided by scale parameter wit set_scale
     Serial.print("FINISHED SETTING UP LOAD ");
     Serial.print(cellID);
@@ -120,89 +94,41 @@ void read_send(HX711 load_cellX){
 //read inputs from load cell and control LEDs accordinly 
 //this is the code that is continuously looping and checking for tension and compression in the load cell. it does not run unless it is called by the loop function, which in this case it is always being called by the loop function
 void read_send(HX711 load_cellX, String cellID){
->>>>>>> Stashed changes
     load_cellX.set_scale(SCALE);   // this value is obtained by calibrating the scale with known weights; see the README for details
     int ledbrightness;             //Integer for the brightness of the LED lights    
     float forceC;
     float forceT;
-<<<<<<< Updated upstream
-=======
-    
->>>>>>> Stashed changes
+
     if (load_cellX.get_units() < NEGATIVE_FLOOR){     // testing for compression in sensor
-      ledbrightness = (map(load_cellX.get_units(), MIN_LB, 0, 255, MAP_LOW)); //Convert the reading from the scale into a pwm output
+      ledbrightness = load_cellX.get_units()*255/MAX_LB; // ledbrightness = (get_units - in_min[0])*(225 - out_min[0]) / (in_max - in_min[0]) + out_min[0]
       forceC = load_cellX.get_units();
       analogWrite(GREEN_LED, ledbrightness);
       analogWrite(RED_LED, 0);
-      Serial.print("COMPRESSION: ");
+      Serial.print("COMPRESSION: \t");
       Serial.print(forceC);
       Serial.print(" lbs");
       Serial.print(" IN LOAD ");
       Serial.print(cellID); 
       Serial.println(); 
     }else if (load_cellX.get_units() > POSITIVE_FLOOR){     //testing for tension in sensor 1
-      ledbrightness = load_cellX.get_units()*255/1.1;
-      //ledbrightness = map(load_cellX.get_units(), 0, MAX_LB, 0, 255);
+      ledbrightness = load_cellX.get_units()*255/MAX_LB; // ledbrightness = (get_units - in_min[0])*(225 - out_min[0]) / (in_max - in_min[0]) + out_min[0]
       forceT = load_cellX.get_units();
       analogWrite(RED_LED, ledbrightness);
       analogWrite(GREEN_LED, 0);
-      Serial.print("TENSION: ");
+      Serial.print("TENSION: \t");
       Serial.print(forceT);
       Serial.print(" lbs");
       Serial.print(" IN LOAD ");
       Serial.print(cellID); 
-      Serial.println(); 
+      Serial.println();
     }else if (NEGATIVE_FLOOR <= load_cellX.get_units() <= POSITIVE_FLOOR){
       analogWrite(GREEN_LED, 0);
       analogWrite(RED_LED, 0);
-<<<<<<< Updated upstream
-      Serial.println("no load detected sensor 1");
-    }
-}
-  
-//void set_calibration(HX711 load_cellX){                         //Refer to this link for the code: https://www.instructables.com/Arduino-Scale-With-5kg-Load-Cell-and-HX711-Amplifi/
-//   buttonState = digitalRead(buttonPin);
-//   massReading = load_cellX.get_units();
-//   if (buttonState == HIGH) {                                     //Check if the pushbutton is pressed. If it is, the buttonState is HIGH (buttonState = 1)
-//    if ((KNOWN_WEIGHT-0.05) <= massReading <= (KNOWN_WEIGHT+0.05) || massReading == 0){
-//      Serial.print("no calibration required");
-//    }else if (massReading < 14.95 || massReading > 15.05){
-//      SCALE == massReading / KNOWN_WEIGHT;
-//    }
-//   }  
-//}
-
-void passiveDelay(int passiveDelayTime){
-  if(millis() - lastUpdate >= passiveDelayTime){ //Check if timer is equal to or later than the collection time variable in config.h (in milliseconds)
-    lastUpdate = millis(); //update old_time to current millis()
-  }
-}
-
-void lcd(){
-/* TODO:
- * Identify LCD pins & wiring
- * Setup LCD library
- * Setup LCD as an output jn the setup function
-*/
-}
-int smoothIt(int from, int to, int val, int power, int reverse) {
-  float to2;
-  to2 = to - from;
-  int ret;
-  if (reverse == 1) {
-    ret = (pow((val - from) / to2 - 1, power) + 1) * to2 + from; //
-    return ret;
-  } else {
-    ret = pow((val - from) / to2, power) * to2 + from; //
-    return ret;
-  }
-}
-=======
       Serial.print("NO LOAD IN LOAD ");
       Serial.print(cellID);
       Serial.println();
-    }
-//    updateLCD(load_cellX);
+    }    
+    updateLCD(load_cellX);
 }
 
 //set_calibration is called by the loop function when the calibration button is pushed.
@@ -210,22 +136,25 @@ int smoothIt(int from, int to, int val, int power, int reverse) {
 //INT_FORCE is the expeceted force in pounds. this varies depending on the configuration of the truss (equilateral triangle or 45, 45, 90 triangle or 30, 60, 90 triangle)
 //CONFIG_FACTOR is set in config.h and is specific to what type of triangle the truss is arranged in
    
-void set_calibration(HX711 load_cellX){                         //Refer to this link for the code: https://www.instructables.com/Arduino-Scale-With-5kg-Load-Cell-and-HX711-Amplifi/
+void set_calibration(HX711 load_cellX){
    float massReading;
    float INT_FORCE;
    load_cellX.set_scale(SCALE);
    massReading = load_cellX.get_units();
    INT_FORCE = KNOWN_WEIGHT / CONFIG_FACTOR;
-   Serial.println(massReading);                                    
-   if ((INT_FORCE-0.05 <= massReading <= INT_FORCE+0.05) || (-0.05 <= massReading <= 0.05))
-   {
-    Serial.println("no calibration needed");
-    lcd.print("no calibration needed");
-   }
-   if (massReading < INT_FORCE-0.05 || massReading > INT_FORCE+0.05){
+   Serial.println(massReading);  
+   
+   String message;                                  
+   
+   if ((INT_FORCE-0.05 <= massReading <= INT_FORCE+0.05) || (-0.05 <= massReading <= 0.05)){
+    message = "no calibration needed";
+    Serial.println(message);
+    lcd.print(message);
+   }else if (massReading < INT_FORCE-0.05 || massReading > INT_FORCE+0.05){
     SCALE = load_cellX.get_value() / INT_FORCE;
-    Serial.println("calibration complete");
-    lcd.print("calibration complete");
+    message = "Calibration completed";
+    Serial.println(message);
+    lcd.print(message);
    }
 }
 
@@ -237,31 +166,29 @@ void setupLCD(){
 }
 
 void updateLCD(HX711 load_cellX){
-//  if(millis() - lastUpdate >= passiveDelayTime){ //Check if timer is equal to or later than the collection time variable in config.h (in milliseconds)
-//    lastUpdate = millis(); //update old_time to current millis()
-//    if (load_cellX.get_units()<NEGATIVE_FLOOR){
-//      lcd.print("compression");
-//      lcd.setCursor(0,1);
-//      lcd.print(load_cellX.get_units());
-//      lcd.setCursor(6,1);
-//      lcd.print("lbs");
-//    }else if (load_cellX.get_units()>POSITIVE_FLOOR){
-//      lcd.print("tension");
-//      lcd.setCursor(0,1);
-//      lcd.print(load_cellX.get_units());
-//      lcd.setCursor(6,1);
-//      lcd.print("lbs");
-//    }else if (NEGATIVE_FLOOR <= load_cellX.get_units() <= POSITIVE_FLOOR){
-//      lcd.print("no load detected");
-//    }
-//    //lcd.print(load_cellX.get_units());
-//    delay(300);
-//    lcd.clear();
-//  }
+  if(millis() - lastUpdate >= passiveDelayTime){ //Check if timer is equal to or later than the collection time variable in config.h (in milliseconds)
+    lastUpdate = millis(); //update old_time to current millis()
+    if (load_cellX.get_units()<NEGATIVE_FLOOR){
+      lcd.print("COMPRESSION");
+      lcd.setCursor(0,1);
+      lcd.print(load_cellX.get_units());
+      lcd.setCursor(6,1);
+      lcd.print("LBS");
+    }else if (load_cellX.get_units()>POSITIVE_FLOOR){
+      lcd.print("TENSION");
+      lcd.setCursor(0,1);
+      lcd.print(load_cellX.get_units());
+      lcd.setCursor(6,1);
+      lcd.print("LBS");
+    }else if (NEGATIVE_FLOOR <= load_cellX.get_units() <= POSITIVE_FLOOR){
+      lcd.print("NO LOAD");
+    }
+    //lcd.print(load_cellX.get_units());
+    delay(300);
+    lcd.clear();
+  }
 }
 
-
->>>>>>> Stashed changes
 
 /* LIBRARIES USED & TUTORIALS
  * 
--- 2021 Code/Truss_Code/config.h
+++ 2021 Code/Truss_Code/config.h
@@ -14,16 +14,6 @@
 
 //DEFINE HX711 LOAD CELL PINS
 //load cell 1
-<<<<<<< Updated upstream
-#define LOADCELL_SCK_PIN1 2
-#define LOADCELL_DT_PIN1 3
-//load cell 2
-#define LOADCELL_SCK_PIN2 4
-#define LOADCELL_DT_PIN2 5
-//Load cell 3
-#define LOADCELL_SCK_PIN3 6
-#define LOADCELL_DT_PIN3 7
-=======
 #define LOADCELL_SCK_PIN1 8
 #define LOADCELL_DT_PIN1 9
 //load cell 2
@@ -32,38 +22,31 @@
 //Load cell 3
 #define LOADCELL_SCK_PIN3 13
 #define LOADCELL_DT_PIN3 12
->>>>>>> Stashed changes
 
 //HX711 LOAD CELL CALIBRATION FACTOR (Note: -7050 worked for 440lb max scale setup. Used -3050 previously)
-//#define CALIBRATION_FACTOR 9779
-//#define SCALE 19590
+//const float SCALE = 19470;
 
-//DELAY CODE (IN MILLISECONDS)
-#define DELAY 0 
+// passive delay for LCD screen
+const long passiveDelayTime = 2000;
 
 //CALIBRATION BUTTON
-#define buttonPin 8           // the pin of the pushbutton
-#define KNOWN_WEIGHT 15       // lbs
+#define buttonPin 52           // the pin of the pushbutton
+const float KNOWN_WEIGHT = 15;       // lbs
+const float CONFIG_FACTOR = 1.00; // 60-60-60 triangle: CONFIG_FACTOR = 1.73, 45-45-90 triangle: CONFIG_FACTOR = ..., 30-60-90 triangle: CONFIG_FACTOR = ....
 
 //ESTIMATED MAX & MIN LOADINGS ON LOAD CELL IN LBS
-//#define MIN_LB -5.0         //largest COMPRESSION load expected on a sensor
-//#define MAX_LB 5.0          //largest TENSION load expected on sensor
+const float MIN_LB = -30.0;         //largest COMPRESSION load expected on a sensor
+const float MAX_LB = 30.0;          //largest TENSION load expected on sensor
 
 //MINIMUM AMOUNT OF LBS TO ACTIVATE LEDS
-//#define MAP_LOW 1           //the lowest the map function will go (may help with delays)
-#define NEGATIVE_FLOOR -0.1    //minimum amount of force before led will activate for tension
-#define POSITIVE_FLOOR 0.1     //minimum amount of force before led will activate for compression
+const float MAP_LOW = 0.0;           //the lowest the map function will go (may help with delays)
+float NEGATIVE_FLOOR = -0.10;    //minimum amount of force before led will activate for tension
+float POSITIVE_FLOOR = 0.10;     //minimum amount of force before led will activate for compression
 
 //DEFINE PINS FOR LED
-<<<<<<< Updated upstream
-#define RED_LED 6           //TENSION
-#define GREEN_LED 7         //COMPRESSION
-#define BLUE_LED 8
-=======
 #define RED_LED 30          //TENSION
 #define GREEN_LED 31        //COMPRESSION
 #define BLUE_LED 32
->>>>>>> Stashed changes
 
 
 #endif
